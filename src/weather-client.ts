144eda76df40bcd8f007a3348e04ee0b4e1c673d
robindelaater <rdelaater@icloud.com>
2022-10-17 17:15:45 +0200

    Make use of geolocation API

--- src/weather-client.ts
+++ src/weather-client.ts
@@ -4,17 +4,32 @@ class WeatherClient {
 
 	constructor(apiKey: string) {
 		this.apiKey = apiKey;
-		this.baseUrl = `${this.baseUrl}?appid=${this.apiKey}&`;
+		this.baseUrl = `${this.baseUrl}?appid=${this.apiKey}`;
 	}
 
 	async getCurrentWeatherByCity(city: string) {
 		try {
-			const res = await fetch(`${this.baseUrl}q=${city}`);
+			const { lat, lon } = await this.getLatLon(city);
+			const res = await fetch(`${this.baseUrl}&lat=${lat}&lon=${lon}`);
 
 			return await res.json();
 		} catch (error) {
 			throw new Error(error);
 		}
+	}
+
+	async getLatLon(input: string) {
+		const res = await fetch(
+			`http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${this.apiKey}`
+		);
+
+		const data = await res.json();
+
+		if (data.length > 0) {
+			return { lat: data[0].lat, lon: data[0].lon };
+		}
+
+		throw new Error('Location not found');
 	}
 }
 
